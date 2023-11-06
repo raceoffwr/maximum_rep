a5ac54723913fe5f6dacaa3def70bcb4352a4ea9
Павел Рогонов <rogonovp8@gmail.com>
2023-11-07 00:42:21 +0300

    Новый код

---
+++ Новый файл.py
@@ -0,0 +1,4 @@
+#Модуль 3, Занятие №2: ”GIT и асимптотическая сложность алгоритмов” - 17.08.2023 - Практика №2
+def is_palindrome(stroke):
+    return stroke == stroke[::-1]
+print(is_palindrome('шалаш'))
