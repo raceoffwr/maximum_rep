be228ae14ecb8dd3caba4cb1548652b47195834d
raceoffwr <rogonovp8@gmail.com>
2023-11-11 15:18:59 +0300

    Create models.py

---
+++ дз 3 модуль 4/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+class Advertisement(models.Model):
+    title = models.CharField(verbose_name="Заголовок", max_length=128)
+    description = models.TextField("Описание")
+    price = models.DecimalField("Цена", max_digits=10, decimal_places=2)  # максимум 99_999_999.99
+    auction = models.BooleanField("Торг", help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = "advertisements"
+# Create your models here.
